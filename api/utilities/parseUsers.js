f5a4c73e897d424ee7cf2c0ea6324a877b5e54ab
Thethickinnchickin <matt.reiley@sjsu.edu>
2024-01-04 15:53:15 -0800

    a

--- api/utilities/parseUsers.js
+++ api/utilities/parseUsers.js
@@ -1,11 +1,11 @@
 const user = require("../models/user");
 const { array } = require("./uploadPhoto");
 
-module.exports = function parseUsers(users, mainUser) {
+module.exports = function parseUsers(mainUser) {
     let removeableUsers = [];
     // for(let user of users) {
 
-    //     for(let followId of mainUser.followers) {
+    //for(let followId of mainUser.followers) {
 
     //             if (user.followers.includes(followId)) {
     //                 if(!removeableUsers.includes(user)) {
@@ -26,29 +26,29 @@ module.exports = function parseUsers(users, mainUser) {
     
     
     //     }
-    //     for(let followId of mainUser.following) {
+        for(let followId of mainUser.following) {
 
-    //         if (user.followers.includes(followId)) {
-    //             if(!removeableUsers.includes(user)) {
+            // if (user.followers.includes(followId)) {
+            //     if(!removeableUsers.includes(user)) {
 
-    //                 removeableUsers.push(user)
-    //             }
-    //         }
+            //         removeableUsers.push(user)
+            //     }
+            // }
                 
             
 
-    //         if(user.following.includes(followId)) {
-    //             if(!removeableUsers.includes(user)) {
+            if(user.following.includes(followId)) {
+                if(!removeableUsers.includes(user)) {
 
-    //                 removeableUsers.push(user)
-    //             }
-    //         } 
+                    removeableUsers.push(user)
+                }
+            } 
                     
 
 
-    // }
+    }
 
-    // }
+    
 
 
 
@@ -62,5 +62,5 @@ module.exports = function parseUsers(users, mainUser) {
     
 
 
-    return users;
+    return removeableUsers;
 }
