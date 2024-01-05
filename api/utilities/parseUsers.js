3173f3fdd59b7c8c02f619dfdf78c02ce2ef2622
Thethickinnchickin <matt.reiley@sjsu.edu>
2024-01-04 16:15:24 -0800

    a

--- api/utilities/parseUsers.js
+++ api/utilities/parseUsers.js
@@ -3,37 +3,37 @@ const { array } = require("./uploadPhoto");
 
 module.exports = function parseUsers(mainUser) {
     let removeableUsers = [];
-    // for(let user of users) {
+    for(let user of users) {
 
-    //for(let followId of mainUser.followers) {
+    for(let followId of mainUser.followers) {
 
-    //             if (user.followers.includes(followId)) {
-    //                 if(!removeableUsers.includes(user)) {
+                if (user.followers.includes(followId)) {
+                    if(!removeableUsers.includes(user)) {
 
-    //                     removeableUsers.push(user)
-    //                 }
-    //             }
+                        removeableUsers.push(user)
+                    }
+                }
                     
                 
 
-    //             if(user.following.includes(followId)) {
-    //                 if(!removeableUsers.includes(user)) {
+                if(user.following.includes(followId)) {
+                    if(!removeableUsers.includes(user)) {
 
-    //                     removeableUsers.push(user)
-    //                 }
-    //             } 
+                        removeableUsers.push(user)
+                    }
+                } 
                         
     
     
-    //     }
+        }
         for(let followId of mainUser.following) {
 
-            // if (user.followers.includes(followId)) {
-            //     if(!removeableUsers.includes(user)) {
+            if (user.followers.includes(followId)) {
+                if(!removeableUsers.includes(user)) {
 
-            //         removeableUsers.push(user)
-            //     }
-            // }
+                    removeableUsers.push(user)
+                }
+            }
                 
             
 
@@ -53,11 +53,11 @@ module.exports = function parseUsers(mainUser) {
 
 
       
-    // for(i=0; i < removeableUsers.length; i++) {
-    //     if(users.indexOf(removeableUsers[i]) !== -1) {
-    //         users.splice(users.indexOf(removeableUsers[i]), 1);
-    //     }
-    // }
+    for(i=0; i < removeableUsers.length; i++) {
+        if(users.indexOf(removeableUsers[i]) !== -1) {
+            users.splice(users.indexOf(removeableUsers[i]), 1);
+        }
+    }
 
     
 
